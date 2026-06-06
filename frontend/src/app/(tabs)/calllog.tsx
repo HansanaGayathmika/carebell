3a6861a5ebb8c40e6c8ea6dff76875155c652b96
Hansana Gayathmika <hansanagayathmika2003@gmail.com>
2026-06-06 21:58:53 +0530

    feat: add CallLogScreen component for displaying call log

---
+++ frontend/src/app/(tabs)/calllog.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function CallLogScreen() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Call Log Screen</Text>
+        </View>
+    )
+}
